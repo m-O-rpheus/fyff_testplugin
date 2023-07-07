caa1477e4de95d25ecb28b473bfa7437f819733a
Markus Jäger <73857179+m-O-rpheus@users.noreply.github.com>
2023-07-07 09:20:48 +0200

    init2

---
+++ plugin_init.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Plugin Name: FYFF TESTPLUGIN
+ * Plugin URI: https://fyff.net
+ * Description: 
+ * Author: Markus Jäger
+ * Author URI: https://markus-jaeger.de
+ * License: GPL
+ * Version: 1.0.0
+ * Update URI:  https://my-domain.com/custom-plugins/plugins-info.json
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+
+
+
+
+if( !function_exists( 'my_plugin_check_for_updates' ) ){
+    
+    function my_plugin_check_for_updates( $update, $plugin_data, $plugin_file ){
+        
+        static $response = false;
+        
+        if( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) )
+            return $update;
+        
+        if( $response === false )
+            $response = wp_remote_get( $plugin_data['UpdateURI'] );
+        
+        if( empty( $response['body'] ) )
+            return $update;
+        
+        $custom_plugins_data = json_decode( $response['body'], true );
+        
+        if( ! empty( $custom_plugins_data[ $plugin_file ] ) )
+            return $custom_plugins_data[ $plugin_file ];
+        else
+            return $update;
+        
+    }
+    
+    add_filter('update_plugins_github.com', 'my_plugin_check_for_updates', 10, 3);
+}
+
+
+
+
+
+
+
+add_action( 'wp_footer', function () {
+
+    echo 'TEST';
+
+}, 10, 0 );
+
+
+
+?>
