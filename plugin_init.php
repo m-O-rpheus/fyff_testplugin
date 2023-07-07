6ab6324121e409b3bcdd64d3d1af46841a1bfc29
Markus Jäger <73857179+m-O-rpheus@users.noreply.github.com>
2023-07-07 11:25:54 +0200

    init

--- plugin_init.php
+++ plugin_init.php
@@ -7,43 +7,49 @@
  * Author URI: https://markus-jaeger.de
  * License: GPL
  * Version: 1.0.0
- * Update URI: https://raw.githubusercontent.com/m-O-rpheus/fyff_testplugin/main/plugins-info.json
+ * Update URI: https://raw.githubusercontent.com/m-O-rpheus/fyff_testplugin/main/plugin_info.json
  */
 
 defined( 'ABSPATH' ) || exit;
 
 
 add_filter('update_plugins_raw.githubusercontent.com', function ( $update, $plugin_data, $plugin_file ) {
-
-    print_r( $plugin_data );
-
-
     
     static $response = false;
     
-    if( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) )
+    if( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) ) {
+
         return $update;
-    
-    if( $response === false )
+    }
+
+    if( $response === false ) {
+
         $response = wp_remote_get( $plugin_data['UpdateURI'] );
-    
-    if( empty( $response['body'] ) )
+    }
+
+    if( empty( $response['body'] ) ) {
+
         return $update;
+    }
+
+    $my_plugin_data = json_decode( $response['body'], true );
+
+
+    print_r( $plugin_data );
+    print_r( $my_plugin_data );
     
-    $custom_plugins_data = json_decode( $response['body'], true );
 
 
+    if( ! empty( $my_plugin_data[ $plugin_file ] ) ) {
 
-    print_r( $custom_plugins_data );
-    
+        return $my_plugin_data[ $plugin_file ];
+    }
+    else {
 
-
-    if( ! empty( $custom_plugins_data[ $plugin_file ] ) )
-        return $custom_plugins_data[ $plugin_file ];
-    else
         return $update;
+    }
     
-}, 10, 3);
+}, 10, 3 );
     
 
 
