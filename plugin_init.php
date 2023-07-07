2348e21316a4b89edecadc6f91927bf2b217a052
Markus Jäger <73857179+m-O-rpheus@users.noreply.github.com>
2023-07-07 10:40:06 +0200

    Update plugin_init.php

--- plugin_init.php
+++ plugin_init.php
@@ -7,25 +7,75 @@
  * Author URI: https://markus-jaeger.de
  * License: GPL
  * Version: 1.0.0
- * Update URI: https://raw.githubusercontent.com/m-O-rpheus/fyff_testplugin/main/plugins-info.json
  */
 
 defined( 'ABSPATH' ) || exit;
 
 
 
-add_filter('update_plugins_raw.githubusercontent.com', function( $update, $plugin_data, $plugin_file, $locales ) {
+
+
+
+
+
+ 
+add_filter( 'update_plugins_wprepository.semiweb.eu', function( $update, array $plugin_data, string $plugin_file, $locales ) {
 
     print_r( $plugin_data );
 
-    // Überprüfen, ob ein neues Update verfügbar ist
-    //if (version_compare($plugin_data['Version'], '1.0.0', '<')) {
-        $update = true;
-    //}
 
-    return $update;
+    /*if ( $plugin_file !== 'whatever-plugin/whatever-plugin.php' ) {
+        
+        return $update;   
+    }*/
 
-}, 10, 4);
+    /*if ( !empty( $update ) ) {
+
+        return $update;
+    }*/
+    
+    
+    $changelog = bbloomer_whatever_plugin_request();
+
+    if ( !version_compare( $plugin_data['Version'], $changelog->latest_version, '<' ) ) {
+
+        return $update;
+    }
+
+
+    print_r( $plugin_data );
+
+
+
+    print_r( array(
+        'slug' => 'fyff_testplugin-main',
+        'version' => $changelog->latest_version,
+        'url' => $plugin_data['PluginURI'],
+        'package' => $changelog->download_url,
+    ) );
+
+    return array(
+        'slug' => 'fyff_testplugin-main',
+        'version' => $changelog->latest_version,
+        'url' => $plugin_data['PluginURI'],
+        'package' => $changelog->download_url,
+    );
+
+}, 9999, 4 );
+ 
+
+ 
+function bbloomer_whatever_plugin_request() {
+    $access = wp_remote_get( 'https://wprepository.semiweb.eu/json.json', array( 'timeout' => 10, 'headers' => array( 'Accept' => 'application/json' ) ) );
+
+    if ( !is_wp_error( $access ) && 200 === wp_remote_retrieve_response_code( $access ) ) {
+
+        $result = json_decode( wp_remote_retrieve_body( $access ) );
+        return $result;      
+    }
+}
+
+
 
 
 
