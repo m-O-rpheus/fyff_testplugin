cf072c5c049e449dd9ed4a0cc2401c7d803fd724
Markus Jäger <73857179+m-O-rpheus@users.noreply.github.com>
2023-07-07 09:38:32 +0200

    init6

--- plugin_init.php
+++ plugin_init.php
@@ -14,34 +14,12 @@ defined( 'ABSPATH' ) || exit;
 
 
 
+add_filter('update_plugins_github.com', function( $update, $plugin_data, $plugin_file, $locales ) {
 
+    return $update;
 
-if( !function_exists( 'my_plugin_check_for_updates' ) ){
-    
-    function my_plugin_check_for_updates( $update, $plugin_data, $plugin_file ){
-        
-        static $response = false;
-        
-        if( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) )
-            return $update;
-        
-        if( $response === false )
-            $response = wp_remote_get( $plugin_data['UpdateURI'] );
-        
-        if( empty( $response['body'] ) )
-            return $update;
-        
-        $custom_plugins_data = json_decode( $response['body'], true );
-        
-        if( ! empty( $custom_plugins_data[ $plugin_file ] ) )
-            return $custom_plugins_data[ $plugin_file ];
-        else
-            return $update;
-        
-    }
-    
-    add_filter('update_plugins_github.com', 'my_plugin_check_for_updates', 10, 3);
-}
+}, 10, 4);
+
 
 
 
